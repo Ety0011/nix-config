e7b9adadd5d1ae3a5884ea5ac4060dde41416c6c
Ety <etienne.orio@orio.ch>
2026-02-13 00:24:50 +0100

    Add python documentation.

---
+++ documentation/python/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "My Python project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          (pkgs.python3.withPackages (pypkgs: with pypkgs; [
+            numpy
+          ]))
+        ];
+
+        shellHook = ''
+          echo "Python development environment"
+          python --version
+        '';
+      };
+    };
+}
