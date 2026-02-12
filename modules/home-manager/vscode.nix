7242767e93692865458f314413bf6f6c2b37422c
Ety <etienne.orio@orio.ch>
2026-02-13 00:20:23 +0100

    Add python vscode extensions.

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -5,7 +5,22 @@
     enable = true;
     profiles.default = {
       extensions = with pkgs.vscode-extensions; [
+        # Nix
         jnoortheen.nix-ide
+        
+        # Python
+        ms-python.python
+        vscode-extension-charliermarsh-ruff
+        # ms-toolsai.jupyter
+
+        # Java
+        # vscjava.vscode-java-pack
+
+        # # C/C++
+        # ms-vscode.cpptools
+
+        # # C#
+        # ms-dotnettools.csharp
       ];
       userSettings = {
         # "editor.fontSize" = 14;
