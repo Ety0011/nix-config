fc8d2209ef3c75534e56c2368293f2edbdba0dab
Ety <etienne.orio@orio.ch>
2026-02-16 20:16:03 +0100

    Accept suggestion of system wide git ignore.

--- hosts/Etiennes-MacBook-Pro/configuration.nix
+++ hosts/Etiennes-MacBook-Pro/configuration.nix
@@ -3,7 +3,6 @@
 {
   # 1. Host-Specific Packages (e.g. Work tools vs Personal tools)
   environment.systemPackages = with pkgs; [
-    vim # IDEA: I suggest moving it to a home-manager package (you can check my neovim.nix in my nix-darwin config)
     colima
     docker
     docker-compose
--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -15,16 +15,12 @@
       pull.rebase = false;
     };
 
-    /*
-      # IDEA: THis is an example on how you can set a system wide git ignore. Basically you don't have to put these in any more gitignore
-      ignores = [
-        ".direnv/"
-        ".envrc"
-        ".venv/"
-        "result"
-        "*.swp"
-        ".DS_Store"
-      ];
-    */
+    # System wide git ignore
+    ignores = [
+      ".direnv/"
+      ".envrc"
+      ".venv/"
+      ".DS_Store"
+    ];
   };
 }
